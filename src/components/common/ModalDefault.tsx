719e4b55d37f235469add8b10f71b814fe84ab4b
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-08-18 00:35:41 +0900

    [feat] #24 modal 백그라운드 조정을 위해 RN Modal 사용

--- src/components/common/ModalDefault.tsx
+++ src/components/common/ModalDefault.tsx
@@ -1,49 +1,46 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Modal} from 'react-native';
 import {useModalStore} from '@/store/store';
 import Button from './Button';
 import ButtonCouple from './ButtonCouple';
 
 const ModalDefault = () => {
   const {modal, closeModal} = useModalStore();
-  if (!modal.isOpen) {
-    return;
-  }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.wrapper}>
-        <Text style={styles.title}>{modal.title}</Text>
-        {modal.content ? (
-          <Text style={styles.content}>{modal.content}</Text>
-        ) : (
-          <View style={{paddingBottom: 12}} />
-        )}
-        {modal.textCancel ? (
-          <ButtonCouple
-            onPressRight={modal.onPress || closeModal}
-            textRight={modal.text}
-            onPressLeft={modal.onPressCancel || closeModal}
-            textLeft={modal.textCancel}
-            disableRight={modal.disable}
-            theme="sub"
-          />
-        ) : (
-          <Button onPress={closeModal} text={modal.text} theme="sub" />
-        )}
+    <Modal animationType="slide" visible={modal.isOpen} transparent={true}>
+      <View style={styles.container}>
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>{modal.title}</Text>
+          {modal.content ? (
+            <Text style={styles.content}>{modal.content}</Text>
+          ) : (
+            <View style={{paddingBottom: 12}} />
+          )}
+          {modal.textCancel ? (
+            <ButtonCouple
+              onPressRight={modal.onPress || closeModal}
+              textRight={modal.text}
+              onPressLeft={modal.onPressCancel || closeModal}
+              textLeft={modal.textCancel}
+              disableRight={modal.disable}
+              theme="sub"
+            />
+          ) : (
+            <Button onPress={closeModal} text={modal.text} theme="sub" />
+          )}
+        </View>
       </View>
-    </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
+    flex: 1,
+    alignContent: 'center',
+    textAlignVertical: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
     justifyContent: 'center',
   },
   wrapper: {
