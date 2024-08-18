1bcf3d2e84c775edd06a1c3d3f6fe1e056277384
Yedam Oh <dhdpekfa1@naver.com>
2024-08-18 12:59:52 +0900

    [style] #28 인풋 공통 컴포넌트 스타일 재수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
 import {commonStyle} from '@/styles/common';
-import InputBox from '@/components/common/InputBox';
-
-import NickNameIconSvg from '@/assets/icons/nickname.svg';
 
 const Home = () => {
-  const [text, setText] = useState('');
-
   return (
     <View style={commonStyle.container}>
       <Text>home</Text>
-      <InputBox
-        value={text}
-        setValue={setText}
-        placeholder={'친구를 검색해보세요.'}
-        icon={NickNameIconSvg}
-      />
     </View>
   );
 };
