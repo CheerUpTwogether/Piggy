9eba3dd70b7823a102e31b11e21ae40d65198602
Yedam Oh <dhdpekfa1@naver.com>
2024-08-19 17:07:58 +0900

    #49 더미 데이터 파일 분리

---
+++ src/mock/Friends/dummy.ts
@@ -0,0 +1,120 @@
+import {User, Friend} from '@/types/Friends';
+
+export const dummy_profile: User = {
+  profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+  nick_name: '움파룸파',
+  email: 'oompaLoompa@gmail.com',
+};
+
+export const dummy_friends_data: Friend[] = [
+  {
+    id: 1,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '일스틴',
+  },
+  {
+    id: 2,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '이스틴',
+  },
+  {
+    id: 3,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '삼스틴',
+  },
+  {
+    id: 4,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '사스틴',
+  },
+  {
+    id: 5,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '오스틴',
+  },
+  {
+    id: 6,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '육스틴',
+  },
+  {
+    id: 7,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '칠스틴',
+  },
+  {
+    id: 8,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '팔스틴',
+  },
+  {
+    id: 9,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '구스틴',
+  },
+  {
+    id: 10,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '심스틴',
+  },
+  {
+    id: 11,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '가스틴',
+  },
+  {
+    id: 12,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '나스틴',
+  },
+  {
+    id: 13,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '다스틴',
+  },
+  {
+    id: 14,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '라스틴',
+  },
+  {
+    id: 15,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '마스틴',
+  },
+  {
+    id: 16,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '바스틴',
+  },
+  {
+    id: 17,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '사스틴',
+  },
+  {
+    id: 18,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '아스틴',
+  },
+  {
+    id: 19,
+    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
+    nick_name: '자스틴',
+  },
+  {
+    id: 20,
+    profile_image_path:
+      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+    nick_name: '차스틴',
+  },
+];
--- src/pages/friends/Friends.tsx
+++ src/pages/friends/Friends.tsx
@@ -8,129 +8,10 @@ import {
   ScrollView,
 } from 'react-native';
 import {commonStyle} from '@/styles/common';
-import {User, Friend} from '@/types/Friends';
 import EmptyResult from '@/components/common/EmptyResult';
+import {dummy_friends_data, dummy_profile} from '@/mock/Friends/dummy';
 
 import MoreSvg from '@/assets/icons/more.svg';
-
-const dummy_profile: User = {
-  profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-  nick_name: '움파룸파',
-  email: 'oompaLoompa@gmail.com',
-};
-
-const dummy_data: Friend[] = [
-  {
-    id: 1,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '일스틴',
-  },
-  {
-    id: 2,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '이스틴',
-  },
-  {
-    id: 3,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '삼스틴',
-  },
-  {
-    id: 4,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '사스틴',
-  },
-  {
-    id: 5,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '오스틴',
-  },
-  {
-    id: 6,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '육스틴',
-  },
-  {
-    id: 7,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '칠스틴',
-  },
-  {
-    id: 8,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '팔스틴',
-  },
-  {
-    id: 9,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '구스틴',
-  },
-  {
-    id: 10,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '심스틴',
-  },
-  {
-    id: 11,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '가스틴',
-  },
-  {
-    id: 12,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '나스틴',
-  },
-  {
-    id: 13,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '다스틴',
-  },
-  {
-    id: 14,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '라스틴',
-  },
-  {
-    id: 15,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '마스틴',
-  },
-  {
-    id: 16,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '바스틴',
-  },
-  {
-    id: 17,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '사스틴',
-  },
-  {
-    id: 18,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '아스틴',
-  },
-  {
-    id: 19,
-    profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '자스틴',
-  },
-  {
-    id: 20,
-    profile_image_path:
-      'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '차스틴',
-  },
-];
 
 const Friends = () => {
   return (
@@ -151,9 +32,9 @@ const Friends = () => {
 
         <View style={styles.friendListWrapper}>
           <Text style={commonStyle.MEDIUM_33_16}>
-            친구 {dummy_data.length}명
+            친구 {dummy_friends_data.length}명
           </Text>
-          {dummy_data.length === 0 ? (
+          {dummy_friends_data.length === 0 ? (
             <View style={styles.emptyWrapper}>
               <EmptyResult
                 reason="추가된 친구가 없어요."
@@ -162,7 +43,7 @@ const Friends = () => {
             </View>
           ) : (
             <View>
-              {dummy_data.map(item => (
+              {dummy_friends_data.map(item => (
                 <View key={item.id} style={styles.friendContainer}>
                   <View style={styles.friendWrapper}>
                     <Image
