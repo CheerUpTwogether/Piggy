6b1ffb02b129eb2ed3239733e98142e67f840b42
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-08-19 14:31:17 +0900

    [styles]#41 공통 스타일 폰트 확립

---
+++ src/styles/font.ts
@@ -0,0 +1,48 @@
+import {TextStyle} from 'react-native';
+
+export const Bold: TextStyle = {
+  fontFamily: 'NotoSansKR-Bold',
+  fontWeight: 'bold',
+};
+
+export const Medium: TextStyle = {
+  fontFamily: 'NotoSansKR-Medium',
+  fontWeight: 'medium',
+};
+
+export const Regular: TextStyle = {
+  fontFamily: 'NotoSansKR-Regular',
+  fontWeight: 'regular',
+};
+
+export const font_primary: TextStyle = {
+  color: '#ED423F',
+};
+
+export const font_sub: TextStyle = {
+  color: '#04BF8A',
+};
+
+export const font_ff: TextStyle = {
+  color: '#FFFFFF',
+};
+
+export const font_ef: TextStyle = {
+  color: '#EFEFEF',
+};
+
+export const font_aa: TextStyle = {
+  color: '#AAAAAA',
+};
+
+export const font_99: TextStyle = {
+  color: '#999999',
+};
+
+export const font_77: TextStyle = {
+  color: '#777777',
+};
+
+export const font_33: TextStyle = {
+  color: '#333333',
+};
