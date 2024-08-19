6fb1af11a293fb50a4444e1257564dbca6c7e9a1
Yedam Oh <dhdpekfa1@naver.com>
2024-08-19 11:41:24 +0900

    [feat] #40 로그인 페이지 화면 구현

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,19 +1,78 @@
-import ToggleButton from 'components/common/ToggleButton';
-import React, {useState} from 'react';
-import {View} from 'react-native';
+import React from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {commonStyle} from '@/styles/common';
+
+import LogoSvg from '@/assets/icons/logo.svg';
+import KakaoSvg from '@/assets/icons/kakao.svg';
 
 const Login = () => {
-  const [isOn, setIsOn] = useState(true);
-
-  const handleToggle = () => {
-    setIsOn(!isOn);
-  };
-
   return (
-    <View>
-      <ToggleButton initialState={isOn} onToggle={handleToggle} />
+    <View style={commonStyle.container}>
+      <View style={style.wrapper}>
+        <LogoSvg style={{marginLeft: 40}} width={160} height={60} />
+        <View style={style.explain}>
+          <Text style={style.startText}>시작하기</Text>
+          <Text style={style.simpleText}>
+            간단한 계정 연동으로 바로 시작해보세요!
+          </Text>
+        </View>
+        <View style={style.loginWrapper}>
+          <TouchableOpacity activeOpacity={0.8} style={style.social}>
+            <KakaoSvg width={33} height={33} />
+            <Text style={style.socialText}>카카오로 로그인</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.helperWrapper}>
+          <Text style={[style.helper, {textDecorationLine: 'underline'}]}>
+            로그인에 문제가 있으신가요?
+          </Text>
+          <Text style={style.helper}>이용 약관</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 70,
+  },
+  explain: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 14,
+    marginTop: 50,
+  },
+  startText: {fontSize: 24, fontWeight: 'bold', color: '#333'},
+  simpleText: {fontSize: 16, fontWeight: 'regular', color: '#AAA'},
+  loginWrapper: {marginTop: 50, gap: 16},
+  social: {
+    backgroundColor: '#FEE500',
+    width: 340,
+    height: 50,
+    borderRadius: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+  },
+  socialText: {
+    fontSize: 14,
+    fontWeight: 'regular',
+    color: '#333',
+    marginLeft: 72,
+  },
+  helperWrapper: {
+    position: 'absolute',
+    bottom: 12,
+    alignItems: 'center',
+    width: '100%',
+    gap: 12,
+  },
+  helper: {fontSize: 14, fontWeight: 'regular', color: '#AAA'},
+});
 
 export default Login;
