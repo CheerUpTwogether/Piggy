ca588b074f2c77465769fedc417116c4b353540d
Yedam Oh <dhdpekfa1@naver.com>
2024-08-20 12:02:43 +0900

    [feat] Friends 페이지에서 검색 페이지로 네비게이션 연결

---
+++ src/pages/friends/FriendSearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const FriendSearch = () => {
+  return (
+    <View>
+      <Text>친구 검색</Text>
+    </View>
+  );
+};
+
+export default FriendSearch;
