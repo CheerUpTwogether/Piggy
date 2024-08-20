ba90aea4408a98863b4594cf21690e790b351234
Yedam Oh <dhdpekfa1@naver.com>
2024-08-20 21:49:23 +0900

    [feat] #65 프로필 상세 모달 등급리스트 구현

--- src/pages/friends/ProfileDetail.tsx
+++ src/pages/friends/ProfileDetail.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+  Dimensions,
+} from 'react-native';
 import {commonStyle} from '@/styles/common';
 import {ProfileDetailProps} from '@/mock/Friends/type';
 
@@ -9,6 +17,35 @@ import TrashSvg from '@/assets/icons/trash.svg';
 import AddFriendSvg from '@/assets/icons/addFriend.svg';
 import EditSvg from '@/assets/icons/edit.svg';
 import UTurnSvg from '@/assets/icons/uTurn.svg';
+
+const {height: screenHeight} = Dimensions.get('window');
+
+const gradeList = [
+  {
+    id: 1,
+    grade: '약속 베이비',
+    explain: '아직 정보가 부족해요.',
+    gradeColor: '#333',
+  },
+  {
+    id: 2,
+    grade: '프로 약속 탈주러',
+    explain: '관심과 노력이 필요해요...',
+    gradeColor: '#ED423F',
+  },
+  {
+    id: 3,
+    grade: '약속 수행러',
+    explain: '약속을 잘 지키는 편이에요!',
+    gradeColor: '#FEE500',
+  },
+  {
+    id: 4,
+    grade: '프로 약속 이행러',
+    explain: '약속은 그 무엇보다 소중해요!',
+    gradeColor: '#04BF8A',
+  },
+];
 
 // 등급 구하는 함수
 const determineGrade = (
@@ -37,10 +74,21 @@ const ProfileDetail: React.FC<ProfileDetailProps> = ({
   profileImagePath,
   friend,
 }) => {
+  const [gradeListShow, setGradeListShow] = useState(false);
+  const slideAnim = useRef(new Animated.Value(screenHeight)).current;
+
   const {grade, gradeColor} = determineGrade(
     totalAppointments,
     completedAppointments,
   );
+
+  useEffect(() => {
+    Animated.timing(slideAnim, {
+      toValue: gradeListShow ? 0 : screenHeight,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [gradeListShow, slideAnim]);
 
   const iconShow = () => {
     if (uuid === '1000') {
@@ -77,15 +125,33 @@ const ProfileDetail: React.FC<ProfileDetailProps> = ({
     }
   };
 
+  const handleGradeToggle = () => {
+    setGradeListShow(!gradeListShow);
+  };
+
   return (
     <View style={commonStyle.CONTAINER}>
-      <Image source={{uri: profileImagePath}} style={styles.profile} />
+      <View>
+        <Image source={{uri: profileImagePath}} style={styles.profile} />
+      </View>
 
       <View style={styles.introduceContainer}>
         <View style={styles.introduceWrapper}>
-          <View style={styles.gradeWrapper}>
-            <GradeSvg style={styles.gradeStyle} color={gradeColor as string} />
-          </View>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={handleGradeToggle}
+            style={
+              gradeListShow ? styles.uTurnIconWrapper : styles.gradeWrapper
+            }>
+            {gradeListShow ? (
+              <UTurnSvg style={styles.uTurnIcon} />
+            ) : (
+              <GradeSvg
+                style={styles.gradeStyle}
+                color={gradeColor as string}
+              />
+            )}
+          </TouchableOpacity>
           <View style={styles.nickNameWrapper}>
             <Text style={commonStyle.BOLD_33_20}>{nick_name}</Text>
             <Text style={commonStyle.MEDIUM_99_14}>{grade}</Text>
@@ -93,6 +159,26 @@ const ProfileDetail: React.FC<ProfileDetailProps> = ({
         </View>
         {iconShow()}
       </View>
+
+      <Animated.View
+        style={[
+          styles.gradeListContainer,
+          {transform: [{translateX: slideAnim}]},
+        ]}>
+        <View>
+          {gradeList.map(item => (
+            <View key={item.id} style={styles.explainWrapper}>
+              <View style={styles.gradeWrapper}>
+                <GradeSvg style={styles.gradeStyle} color={item.gradeColor} />
+              </View>
+              <View style={{gap: 4}}>
+                <Text style={commonStyle.MEDIUM_33_18}>{item.grade}</Text>
+                <Text style={commonStyle.REGULAR_99_16}>{item.explain}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </Animated.View>
     </View>
   );
 };
@@ -127,6 +213,49 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   rightIcon: {width: 28, height: 28, color: '#555'},
+  uTurnIconWrapper: {
+    borderColor: '#EFEFEF',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  uTurnIcon: {
+    width: 20,
+    height: 20,
+    color: '#ED423F',
+    marginBottom: 4,
+  },
+  gradeListContainer: {
+    position: 'absolute',
+    top: 16,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 380,
+    marginHorizontal: 16,
+    backgroundColor: '#FFF',
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    borderColor: '#EFEFEF',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gradeListContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  explainWrapper: {
+    flexDirection: 'row',
+    marginBottom: 12,
+    alignItems: 'center',
+    gap: 14,
+  },
 });
 
 export default ProfileDetail;
