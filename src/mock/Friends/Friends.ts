e58de5c561c1774ebe5fffeb06700d30827a671d
Yedam Oh <dhdpekfa1@naver.com>
2024-08-20 13:46:37 +0900

    [feat] 검색어에 따라 하단에 정렬 기능 구현

--- src/mock/Friends/Friends.ts
+++ src/mock/Friends/Friends.ts
@@ -10,111 +10,131 @@ export const dummy_friends_data: Friend[] = [
   {
     id: 1,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '일스틴',
+    nick_name: 'Zi존 강현',
+    friend: true,
   },
   {
     id: 2,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '이스틴',
+    nick_name: '진정준',
+    friend: false,
   },
   {
     id: 3,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '삼스틴',
+    nick_name: '박상형',
+    friend: true,
   },
   {
     id: 4,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '사스틴',
+    nick_name: '우빛나',
+    friend: true,
   },
   {
     id: 5,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '오스틴',
+    nick_name: '박지원',
+    friend: true,
   },
   {
     id: 6,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '육스틴',
+    nick_name: '여다희',
+    friend: false,
   },
   {
     id: 7,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '칠스틴',
+    nick_name: '김소현',
+    friend: false,
   },
   {
     id: 8,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '팔스틴',
+    nick_name: '이가은',
+    friend: true,
   },
   {
     id: 9,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '구스틴',
+    nick_name: '김설희',
+    friend: true,
   },
   {
     id: 10,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '심스틴',
+    nick_name: '김한령',
+    friend: false,
   },
   {
     id: 11,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '가스틴',
+    nick_name: '김연우',
+    friend: true,
   },
   {
     id: 12,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '나스틴',
+    nick_name: '대앙마마',
+    friend: true,
   },
   {
     id: 13,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '다스틴',
+    nick_name: '닉네임_뭐하지?',
+    friend: true,
   },
   {
     id: 14,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '라스틴',
+    nick_name: '길게쓴_닉네임',
+    friend: true,
   },
   {
     id: 15,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '마스틴',
+    nick_name: '마수현',
+    friend: true,
   },
   {
     id: 16,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '바스틴',
+    nick_name: '고정민',
+    friend: true,
   },
   {
     id: 17,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '사스틴',
+    nick_name: '전수상',
+    friend: true,
   },
   {
     id: 18,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '아스틴',
+    nick_name: '부정맥놉',
+    friend: true,
   },
   {
     id: 19,
     profile_image_path: 'https://i.pravatar.cc/250?u=mail@ashallendesign.co.uk',
-    nick_name: '자스틴',
+    nick_name: 'en_nick',
+    friend: true,
   },
   {
     id: 20,
     profile_image_path:
       'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
-    nick_name: '차스틴',
+    nick_name: 'Jonver',
+    friend: true,
   },
 ];
--- src/pages/friends/FriendSearch.tsx
+++ src/pages/friends/FriendSearch.tsx
@@ -1,13 +1,58 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-// import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  StyleSheet,
+} from 'react-native';
 import {commonStyle} from '@/styles/common';
 import InputBox from '@/components/common/InputBox';
+import {dummy_friends_data} from '@/mock/Friends/Friends';
 
 import SearchFriendSvg from '@/assets/icons/searchFriend.svg';
+import AddFriendSvg from '@/assets/icons/addFriend.svg';
+import EmptyResult from '@/components/common/EmptyResult';
 
 const FriendSearch = () => {
   const [keyword, setKeyword] = useState('');
+
+  const filterFriend = keyword
+    ? dummy_friends_data
+        .filter(friend =>
+          friend.nick_name.toLowerCase().includes(keyword.toLowerCase()),
+        )
+        .sort((a, b) => {
+          const nameA = a.nick_name.toLowerCase();
+          const nameB = b.nick_name.toLowerCase();
+          return nameA.localeCompare(nameB, 'ko');
+        })
+    : [];
+
+  const friends = filterFriend.filter(friend => friend.friend);
+  const nonFriends = filterFriend.filter(friend => !friend.friend);
+
+  const sortedData = [...friends, ...nonFriends];
+  const renderItem = ({item}) => (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={styles.friendContainer}
+      onPress={() => console.log('TODO: 프로필 상세 모달')}>
+      <View style={styles.friendWrapper}>
+        <Image
+          source={{uri: item.profile_image_path}}
+          style={styles.friendProfile}
+        />
+        <Text style={commonStyle.MEDIUM_33_14}>{item.nick_name}</Text>
+      </View>
+      {item.friend ? (
+        <View />
+      ) : (
+        <AddFriendSvg width={18} height={18} color={'#333'} />
+      )}
+    </TouchableOpacity>
+  );
 
   return (
     <View style={commonStyle.CONTAINER}>
@@ -19,8 +64,41 @@ const FriendSearch = () => {
         isLarge={false}
         goBack={true}
       />
+      <FlatList
+        data={sortedData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        ListEmptyComponent={
+          <EmptyResult
+            reason="검색 결과가 없어요."
+            solution="올바른 닉네임을 검색해보세요"
+          />
+        }
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  friendContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: '#fff',
+    zIndex: 1,
+  },
+  friendWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  friendProfile: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+  },
+});
 
 export default FriendSearch;
