162806c7f80bf51d7e107cbf9eff44b22bd569cc
mymain838 <hades517111@gmail.com>
2024-08-21 01:13:45 +0900

    [feat] #68 #72 #73 설정, 문의, 프로필 수정 화면 퍼블리싱

---
+++ src/components/setting/EditProfile.tsx
@@ -0,0 +1,97 @@
+import {commonStyle} from '@/styles/common';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import InputBox from '../common/InputBox';
+import {useEffect, useState} from 'react';
+import {useRoute} from '@react-navigation/native';
+import NickNameSvg from '@/assets/icons/nickname.svg';
+import CameraSvg from '@/assets/icons/camera.svg';
+import Button from '../common/Button';
+
+const EditProfile = () => {
+  const [nickNameValue, setNickNameValue] = useState('');
+  const [iserror, setIsError] = useState(true);
+  const route = useRoute();
+  const {nick_name, profile_image_path} = route.params;
+
+  useEffect(() => {
+    setNickNameValue(nick_name);
+  }, []);
+
+  return (
+    <View style={commonStyle.CONTAINER}>
+      <View>
+        <View style={{alignItems: 'center', marginVertical: 48}}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.profileImgContainer}>
+            <Image
+              source={{uri: profile_image_path}}
+              style={styles.profileImg}
+            />
+            <View style={styles.cameraContainer}>
+              <CameraSvg />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{gap: 8}}>
+          <Text style={commonStyle.MEDIUM_33_16}>닉네임</Text>
+
+          <InputBox
+            value={nickNameValue}
+            setValue={setNickNameValue}
+            placeholder={'수정하실 닉네임을 입력해주세요.'}
+            icon={NickNameSvg}
+            isLarge={true}
+          />
+        </View>
+        {iserror && (
+          <Text style={{...commonStyle.MEDIUM_PRIMARY_12, marginVertical: 8}}>
+            *닉네임은 8글자까지 설정할 수 있습니다
+          </Text>
+        )}
+      </View>
+      <View style={{marginTop: 232, gap: 14, marginHorizontal: 8}}>
+        <Button text="저장" onPress={() => console.log('저장')} />
+        <Button
+          text="로그 아웃"
+          theme="sub"
+          onPress={() => console.log('로그 아웃')}
+        />
+        <TouchableOpacity>
+          <Text style={{...commonStyle.REGULAR_AA_14, textAlign: 'center'}}>
+            회원탈퇴
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileImgContainer: {
+    width: 140,
+    height: 140,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileImg: {
+    width: 120,
+    height: 120,
+    borderRadius: 120,
+    marginVertical: 30,
+  },
+  cameraContainer: {
+    width: 34,
+    height: 34,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    borderRadius: 24,
+    backgroundColor: '#333',
+  },
+});
+
+export default EditProfile;
