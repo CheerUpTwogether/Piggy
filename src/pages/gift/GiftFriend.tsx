39038f889a9c63837000a4327592b7457b25dacb
Yedam Oh <dhdpekfa1@naver.com>
2024-08-21 15:02:55 +0900

    [feat] 선물하기 - 친구 검색 페이지 퍼블리싱

--- src/pages/gift/GiftFriend.tsx
+++ src/pages/gift/GiftFriend.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {commonStyle} from '@/styles/common';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '@/types/Router';
+
+import SearchFriend from '@/assets/icons/searchFriend.svg';
+
+type NavigationProp = StackNavigationProp<RootStackParamList, 'FriendSearch'>;
 
 const GiftFriend = () => {
+  const navigation = useNavigation<NavigationProp>();
+  const handleMoveToFriendSearch = () => {
+    navigation.navigate('FriendSearch');
+  };
+
   return (
-    <View>
-      <Text>GiftFriend</Text>
+    <View style={[commonStyle.CONTAINER, styles.container]}>
+      <View style={styles.wrapper}>
+        <View style={styles.textWrapper}>
+          <Text style={commonStyle.BOLD_33_20}>어떤 친구에게 </Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={commonStyle.BOLD_PRIMARY_20}>Piggy</Text>
+            <Text style={commonStyle.BOLD_33_20}>를 선물할까요?</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={styles.findFriends}
+          onPress={handleMoveToFriendSearch}>
+          <View style={styles.iconWrapper}>
+            <SearchFriend width={64} height={64} color={'#777'} />
+          </View>
+          <Text style={[commonStyle.MEDIUM_AA_20, {textAlign: 'center'}]}>
+            친구 찾기
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 120,
+  },
+  wrapper: {justifyContent: 'center', alignItems: 'center'},
+  textWrapper: {gap: 4, justifyContent: 'center', alignItems: 'center'},
+  findFriends: {marginVertical: 60, gap: 16},
+  iconWrapper: {
+    width: 160,
+    height: 160,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default GiftFriend;
