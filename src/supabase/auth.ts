7c8f59a079d89d6ac5879cdeab0d45b97ec939ad
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-08-30 16:14:52 +0900

    [refactor]console.log 삭제

--- src/supabase/auth.ts
+++ src/supabase/auth.ts
@@ -2,8 +2,6 @@ import supabase from '@/supabase/supabase';
 
 // 카카오 로그인
 export const kakaoLoginSPB = (idToken, accessToken) => {
-  console.log(idToken);
-  console.log(accessToken);
   return supabase.auth.signInWithIdToken({
     provider: 'kakao',
     token: idToken,
