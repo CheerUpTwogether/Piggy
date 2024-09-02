38e7c77e5fc9f2dd57c1fe7463d94928e4decf63
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-09-02 20:29:58 +0900

    [feat]#173 약속 생성 api 연동

--- src/pages/home/AppointmentForm.tsx
+++ src/pages/home/AppointmentForm.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useState} from 'react';
 import {View, StyleSheet, Text, Dimensions, SafeAreaView} from 'react-native';
-import {useFocusEffect} from '@react-navigation/native';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import {commonStyle} from '@/styles/common';
 import ProgressBar from '@/components/common/ProgressBar';
 import ButtonCouple from '@/components/common/ButtonCouple';
@@ -10,7 +10,14 @@ import AppointmentPlace from '@/components/appointment/AppointmentPlace';
 import AppointmentCalendar from '@/components/appointment/AppointmentCalendar';
 import AppointmentPenalty from '@/components/appointment/AppointmentPenalty';
 import AppointmentCheck from '@/components/appointment/AppointmentCheck';
-import {useAppointmentForm} from '@/store/store';
+import {useAppointmentForm, useToastStore, useUserStore} from '@/store/store';
+import {
+  setAppointmentParticipantsSpb,
+  setAppointmentProposerSpb,
+  setAppointmentSpb,
+} from '@/supabase/appointmentSpb';
+import {changeDateText} from '@/utils/timePicker';
+import {AppointmentInsert} from '@/types/appointment';
 
 const {height: screenHeight} = Dimensions.get('screen');
 
@@ -18,6 +25,9 @@ const AppointmentForm = () => {
   const [nowStep, setNowStep] = useState(1);
   const totalStep = 5;
   const {appointmentForm, resetAppointmentForm} = useAppointmentForm();
+  const addToast = useToastStore(state => state.addToast);
+  const {userData} = useUserStore();
+  const navigation = useNavigation();
 
   useFocusEffect(
     useCallback(() => {
@@ -25,13 +35,95 @@ const AppointmentForm = () => {
     }, []),
   );
 
+  const addAppointmentParticipants = async id => {
+    const participants_uuid = [
+      ...appointmentForm.appointment_participants_list.map(el => el.id),
+      userData.id,
+    ];
+
+    const {data, error} = await setAppointmentParticipantsSpb(
+      id,
+      participants_uuid,
+    );
+
+    if (error) {
+      throw Error;
+    }
+    return data;
+  };
+
+  const handleAddAppointment = async () => {
+    try {
+      const data = await addAppointment();
+      await addAppointmentParticipants(data?.[0].id);
+      await updateAppointmentProposer(userData.id, data?.[0].id);
+      navigation.goBack();
+      addToast({
+        success: false,
+        text: '약속을 생성했어요',
+      });
+    } catch {
+      addToast({
+        success: false,
+        text: '약속 생성에 실패했어요',
+      });
+    }
+  };
+
+  const updateAppointmentProposer = async (userId, appointmentId) => {
+    const {data, error} = await setAppointmentProposerSpb(
+      userId,
+      appointmentId,
+    );
+    console.log(data);
+    console.log(error);
+    if (error) {
+      throw Error;
+    }
+  };
+
+  const addAppointment = async () => {
+    const {
+      subject,
+      appointment_participants_list,
+      address,
+      place_name,
+      latitude,
+      longitude,
+      deal_piggy_count,
+      date,
+      time,
+    } = appointmentForm;
+
+    const timeString = `${changeDateText(time.getHours())}:${changeDateText(
+      time.getMinutes(),
+    )}:00`;
+
+    const param: AppointmentInsert = {
+      id: userData.id,
+      subject,
+      participant_count: (appointment_participants_list?.length || 0) + 1,
+      address,
+      place_name,
+      latitude,
+      longitude,
+      appointment_date: `${String(date)} ${timeString}`,
+      deal_piggy_count,
+    };
+    const {data, error} = await setAppointmentSpb(param);
+    if (error) {
+      throw Error;
+    }
+    return data;
+  };
   const handleNext = () => {
     if (nowStep === 5) {
       console.log('TODO: 약속 생성 api 호출');
+      handleAddAppointment();
     }
-
     setNowStep(prevStep => Math.min(prevStep + 1, totalStep));
   };
+
   const handlePrevious = () => {
     setNowStep(prevStep => Math.max(prevStep - 1, 1));
   };
