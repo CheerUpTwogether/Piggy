c1a09fcce08d2b8bf5a5940e73db10295f9cf0e4
AmitaWhite <amitawhite@AmitaWhites-MacBook-Pro-13.local>
2024-09-02 14:58:26 +0900

    [feat] FriendsSpb 추가 : 친구내에서만 검색2 동일함수명 수정

--- src/supabase/FriendsSpb.tsx
+++ src/supabase/FriendsSpb.tsx
@@ -46,8 +46,8 @@ export const getUsersSpb = async (id: string, keyword: string) => {
   }
 };
 
-// 친구 리스트 검새
-export const getFriendsSpb = async (id: string, keyword: string) => {
+// 친구 리스트 검색
+export const getSearchFriendsSpb = async (id: string, keyword: string) => {
   try {
     const {data, error} = await supabase.rpc('select_friends_list', {
       user_uuid: id,
