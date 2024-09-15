6cac3e18d3ba7e733ede457dacc30b5b8ed841fe
Yedam Oh <dhdpekfa1@naver.com>
2024-09-15 13:54:44 +0900

    [feat] #250 카테고리에 따른 이미지 추가

--- src/components/piggy/PiggyUsageItem.tsx
+++ src/components/piggy/PiggyUsageItem.tsx
@@ -3,6 +3,7 @@ import {Image, StyleSheet, Text, View} from 'react-native';
 import dayjs from 'dayjs';
 import {color_ef, commonStyle} from '@/styles/common';
 import {PiggyUsageHistoryProps} from '@/types/gift';
+import LogoSvg from '@/assets/icons/topLogo.svg';
 import ProfileSvg from '@/assets/icons/basicProfile.svg';
 import AppointmentSvg from '@/assets/icons/appointment.svg';
 import CancelSvg from '@/assets/icons/appointmentDelete.svg';
@@ -12,13 +13,16 @@ import LocationFailureSvg from '@/assets/icons/locationFailure.svg';
 
 const PiggyUsageItem = ({item}: {item: PiggyUsageHistoryProps}) => {
   const getShortTitle = (title?: string) => {
-    return title && title.length > 9 ? `${title.slice(0, 10)}...` : title || '';
+    return title && title.length > 9 ? `${title.slice(0, 16)}...` : title || '';
   };
 
   const explain = () => {
     switch (item.changed_category) {
       case '가입':
-        return {title: `${item.contents?.f1}`};
+        return {
+          title: `${item.contents?.f1}`,
+          img: <LogoSvg width={42} height={42} color={'#333'} />,
+        };
       case '선물줌':
         return {title: `${item.contents?.f1}님에게 선물`};
       case '선물받음':
@@ -26,35 +30,38 @@ const PiggyUsageItem = ({item}: {item: PiggyUsageHistoryProps}) => {
       case '벌금(+)':
         return {
           title: `${getShortTitle(item.contents?.f1)}`,
-          imgText: '약속 성공',
-          img: <LocationSuccessSvg />,
+          imgText: '약속성공',
+          img: <LocationSuccessSvg width={24} height={24} color={'#333'} />,
         };
       case '벌금(-)':
         return {
           title: `${getShortTitle(item.contents?.f1)}`,
           imgText: '벌금',
-          img: <LocationFailureSvg />,
+          img: <LocationFailureSvg width={24} height={24} color={'#333'} />,
         };
       case '약속생성을 위한 선 지급':
         return {
           title: `${getShortTitle(item.contents?.f1)}`,
           imgText: '보증금',
-          img: <AppointmentSvg />,
+          img: <AppointmentSvg width={24} height={24} color={'#333'} />,
         };
       case '약속이 생성되지 않음 - 반환':
         return {
           title: `${getShortTitle(item.contents?.f1)}`,
-          imgText: '약속 취소',
-          img: <CancelSvg />,
+          imgText: '약속취소',
+          img: <CancelSvg width={24} height={24} color={'#333'} />,
         };
       case '구매':
         return {
           title: `${item.contents?.f3}`,
           imgText: '상점 구매',
-          img: <BarcodeSvg />,
+          img: <BarcodeSvg width={24} height={24} color={'#333'} />,
         };
       default:
-        return {title: '알수없음'};
+        return {
+          title: '알수없음',
+          img: <LogoSvg width={48} height={48} color={'#333'} />,
+        };
     }
   };
 
@@ -67,10 +74,12 @@ const PiggyUsageItem = ({item}: {item: PiggyUsageHistoryProps}) => {
     return dayjs(dateString).format('YYYY년 MM월 DD일 HH:mm');
   };
 
+  const explanation = explain();
+
   return (
-    <View style={{...styles.container}}>
+    <View style={styles.container}>
       {/* 이미지 */}
-      <View style={{flexDirection: 'row'}}>
+      <View style={styles.wrapper}>
         <View
           style={{
             ...styles.imgArea,
@@ -83,18 +92,19 @@ const PiggyUsageItem = ({item}: {item: PiggyUsageHistoryProps}) => {
                 source={{uri: uri}}
                 style={styles.img}
                 resizeMode="cover"
-                alt="freindsProfile"
+                alt="friendsProfile"
               />
             ) : (
               <ProfileSvg style={styles.img} width={28} height={28} />
             )
           ) : (
-            <View>
-              <Image
-                source={require('@/assets/icons/topLogo.png')}
-                style={styles.img}
-                alt="topLogo"
-              />
+            <View style={styles.imgWrapper}>
+              {explanation.img}
+              {explanation.imgText && (
+                <Text style={commonStyle.MEDIUM_33_12}>
+                  {explanation.imgText}
+                </Text>
+              )}
             </View>
           )}
         </View>
@@ -134,9 +144,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 4,
     justifyContent: 'space-between',
   },
+  wrapper: {flexDirection: 'row', alignItems: 'center'},
   imgArea: {
-    width: 52,
-    height: 52,
+    width: 60,
+    height: 60,
     borderRadius: 100,
     justifyContent: 'center',
     alignItems: 'center',
@@ -145,8 +156,8 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   img: {
-    width: 48,
-    height: 48,
+    width: 54,
+    height: 54,
     resizeMode: 'contain',
     borderRadius: 100,
   },
@@ -156,6 +167,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     alignSelf: 'flex-end',
   },
+  imgWrapper: {gap: 2, alignItems: 'center', justifyContent: 'center'},
 });
 
 export default PiggyUsageItem;
