bcc398d3978a11d7c4de82b71ebde7c2d91cb1a5
mymain838 <hades517111@gmail.com>
2024-09-19 18:47:06 +0900

    [feat] #254 보관함 및 이메일/회원가입 로직 제거

--- src/components/frame/TopTab.tsx
+++ src/components/frame/TopTab.tsx
@@ -21,7 +21,6 @@ import {useUserStore} from '@/store/store';
 import {getPiggySpb} from '@/supabase/AuthSpb';
 import AlertSvg from '@/assets/icons/alert.svg';
 import SearchSvg from '@/assets/icons/search.svg';
-import GoodsBoxSvg from '@/assets/icons/goodsBox.svg';
 import BackSvg from '@/assets/icons/leftArrow.svg';
 import EditSvg from '@/assets/icons/edit.svg';
 import GiftSvg from '@/assets/icons/gift.svg';
@@ -117,10 +116,6 @@ const RightItems = ({name}: {name: string}) => {
             onPress={() => navigation.navigate('GiftFriend')}>
             <GiftSvg style={styles.svg} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.icon}>
-            <GoodsBoxSvg style={styles.svg} />
-          </TouchableOpacity>
-
           <Alarm />
         </View>
       );
@@ -147,11 +142,6 @@ const RightItems = ({name}: {name: string}) => {
             <Text style={styles.text}>{userData.piggy}</Text>
             <Text style={[styles.text, styles.colorRed]}>P</Text>
           </View>
-          <TouchableOpacity
-            style={styles.icon}
-            onPress={() => navigation.navigate('GoodsStorage')}>
-            <GoodsBoxSvg style={styles.svg} />
-          </TouchableOpacity>
           <Alarm />
         </View>
       );
