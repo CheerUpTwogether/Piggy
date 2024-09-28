2d2dcefe6fc9fb4b6c280c736b78a3f2622b06e0
Yedam Oh <dhdpekfa1@naver.com>
2024-09-28 17:09:19 +0900

    [design] #276 친구 페이지 ButtonBottomSheet 상단 줄 제거

--- src/components/common/BottomSheet.tsx
+++ src/components/common/BottomSheet.tsx
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
   },
 
   bottomSheetTopBarWrapper: {
-    height: 44,
+    height: 40,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFF',
