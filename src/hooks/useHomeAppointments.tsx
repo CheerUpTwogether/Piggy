9e77220f02f17f1c49ab559b4b9b9c31b17fb819
Yedam Oh <dhdpekfa1@naver.com>
2024-10-17 16:53:33 +0900

    [!hotfix] 빌드 오류 해결

--- src/hooks/useHomeAppointments.tsx
+++ src/hooks/useHomeAppointments.tsx
@@ -67,7 +67,6 @@ const useHomeAppointments = () => {
     fetchInitialAppointments();
   }, []);
 
-
   // TODO: 임시 - 해당 컴포넌트에서 useEffect 로 값을 줄 순 있지만 충분한 모듈화가 되지 않음. 재사용성 감소. 개선 고민필요
   const configLimit = (n: number) => {
     setLimit(n);
@@ -79,11 +78,11 @@ const useHomeAppointments = () => {
       setOffset(currentPage * limit);
       setLoading(true);
     }
+  };
 
   const fetchInitialAppointments = async () => {
-    await getAppointment(sort);
+    await getAppointment(sort, limit, offset);
     setInitialLoading(false);
-
   };
 
   const createButtonList = () => {
